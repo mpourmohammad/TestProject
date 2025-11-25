d9606934aa781f14066a82ed464a211445b7bbd2
m.pourmohammad74 <m.pourmohammad74@gmail.com>
2025-11-25 14:59:06 +0330

    change refrencType

--- src/RefrenceType.java
+++ src/RefrenceType.java
@@ -1,8 +1,12 @@
+import java.awt.*;
 import java.util.Date;
 
 public class RefrenceType {
     public static void main(String[] args) {
-        Date now = new Date();
-        System.out.println(now.getTime());
+        Point p1 = new Point(1, 1);
+        Point p2 = p1;
+
+        p1.x = 2;
+        System.out.println(p2);
     }
 }
