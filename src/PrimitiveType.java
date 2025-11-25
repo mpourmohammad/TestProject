43bff5084180fc7fe4e130ece2b0018eccd7b4f6
m.pourmohammad74 <m.pourmohammad74@gmail.com>
2025-11-25 12:33:49 +0330

    one commit

---
+++ src/PrimitiveType.java
@@ -0,0 +1,23 @@
+public class PrimitiveType {
+    public static void main(String[] args) {
+        // number
+        // byte -->  1 byte     -128 -- 127
+        // short --> 2 byte     -32K -- 32K
+        // int --> 4 byte       -2b -- 2b
+        // long --> 8 byte
+
+        // float --> 4 byte
+        // double --> 8 byte
+
+        double a = 3.4;
+        System.out.println(a);
+
+        long number = 552443542453L;
+        float f = 3.14f;
+
+        // char a
+
+        // true - false
+        // boolean 1 byte    (true or false)
+    }
+}
