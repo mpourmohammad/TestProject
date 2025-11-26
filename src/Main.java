234709f1f1de8263ee3f757d7ae046582c12134d
m.pourmohammad74 <m.pourmohammad74@gmail.com>
2025-11-26 09:37:43 +0330

    one commit

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,15 @@
 import java.awt.*;
+import java.security.spec.RSAOtherPrimeInfo;
 
 public class Main {
     public static void main(String[] args) {
         String message = "Hello word";
         System.out.println(message.trim());
+
+        final float pi = 3.14F;
+
+        int a = 1;
+        int y = ++a;
+        System.out.printf(String.valueOf(y));
     }
 }
