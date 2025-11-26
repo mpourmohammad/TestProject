5a47fd3943803aac7361c0ebe1656ad7393dc226
m.pourmohammad74 <m.pourmohammad74@gmail.com>
2025-11-26 09:19:40 +0330

    add array

---
+++ src/ArrayTest.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+
+public class ArrayTest {
+    public static void main(String[] args) {
+        int[][] numbers = {{1, 2, 3}, {8, 9, 10}};
+        System.out.println(Arrays.deepToString(numbers));
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
-
+        String message = "Hello word";
+        System.out.println(message.trim());
     }
 }
