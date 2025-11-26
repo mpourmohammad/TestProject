8858f3f7ab86c14dbbc555cc6c4e7e47e4679e7b
m.pourmohammad74 <m.pourmohammad74@gmail.com>
2025-11-26 11:51:14 +0330

    add array

--- src/NumberFormattest.java
+++ src/NumberFormattest.java
@@ -1,4 +1,8 @@
-package PACKAGE_NAME;
+import java.text.NumberFormat;
 
 public class NumberFormattest {
+    public static void main(String[] args) {
+        NumberFormat currency =  NumberFormat.getCurrencyInstance();
+        System.out.print(currency.format(352342));
+    }
 }
