f9a780dc59ddb7c48d387cca0bf5195de211837a
m.pourmohammad74 <m.pourmohammad74@gmail.com>
2025-11-26 11:44:50 +0330

    one commit

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,21 @@ import java.security.spec.RSAOtherPrimeInfo;
 
 public class Main {
     public static void main(String[] args) {
-        String message = "Hello word";
-        System.out.println(message.trim());
+//        String message = "Hello word";
+//        System.out.println(message.trim());
+//
+//        final float pi = 3.14F;
+//
+//        int a = 1;
+//        int y = ++a;
+//        System.out.printf(String.valueOf(y));
+//
+//        String c = "2.5";
+//
+//        double f = Double.parseDouble(c) + 1;
+//        System.out.println(f);
 
-        final float pi = 3.14F;
-
-        int a = 1;
-        int y = ++a;
-        System.out.printf(String.valueOf(y));
+        int h = (int) (Math.random() * 100);
+        System.out.println(h);
     }
 }
