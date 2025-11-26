6fdc71f166980f3f21d155c8a237456b3e3a7098
m.pourmohammad74 <m.pourmohammad74@gmail.com>
2025-11-26 11:57:58 +0330

    add array

--- src/ScannerTest.java
+++ src/ScannerTest.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class ScannerTest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Age:");
+        byte age = sc.nextByte();
+        System.out.println("Name:");
+        String name = sc.next();
+
+        System.out.println("You are Age :" + age + "| And Name is : " + name);
+    }
 }
