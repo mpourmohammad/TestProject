97c7f964bc50ddad673436eb612e81fd32f1dc60
m.pourmohammad74 <m.pourmohammad74@gmail.com>
2025-11-26 14:42:26 +0330

    Practice

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,20 @@
-import java.awt.*;
-import java.security.spec.RSAOtherPrimeInfo;
-
 public class Main {
     public static void main(String[] args) {
-//        String message = "Hello word";
-//        System.out.println(message.trim());
-//
-//        final float pi = 3.14F;
-//
-//        int a = 1;
-//        int y = ++a;
-//        System.out.printf(String.valueOf(y));
-//
-//        String c = "2.5";
-//
-//        double f = Double.parseDouble(c) + 1;
-//        System.out.println(f);
+        String message = "Hello word";
+        System.out.println(message.trim());
+
+        final float pi = 3.14F;
+
+        System.out.println(pi);
+
+        int a = 1;
+        int y = ++a;
+        System.out.printf(String.valueOf(y));
+
+        String c = "2.5";
+
+        double f = Double.parseDouble(c) + 1;
+        System.out.println(f);
 
         int h = (int) (Math.random() * 100);
         System.out.println(h);
--- src/Practice.java
+++ src/Practice.java
@@ -1,4 +1,63 @@
-package PACKAGE_NAME;
+import java.awt.*;
+import java.util.Scanner;
 
 public class Practice {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("یک عدد وارد کنید : ");
+        int number  = input.nextInt();
+
+        int choice = 0;
+        while (choice != 4) {
+            System.out.println("\n--- منو ---");
+            System.out.println("1 - تشخیص مثبت/منفی/صفر");
+            System.out.println("2 - تشخیص زوج یا فرد");
+            System.out.println("3 - محاسبه فاکتوریل");
+            System.out.println("4 - خروج");
+            System.out.print("گزینه را وارد کنید: ");
+
+            choice = input.nextInt();
+
+            switch (choice) {
+                case 1:
+                    if (number > 0) {
+                        System.out.println("عدد مثبت است.");
+                    } else if (number < 0) {
+                        System.out.println("عدد منفی است.");
+                    } else {
+                        System.out.println("عدد صفر است.");
+                    }
+                    break;
+
+                case 2:
+                    if (number % 2 == 0) {
+                        System.out.println("عدد زوج است.");
+                    } else {
+                        System.out.println("عدد فرد است.");
+                    }
+                    break;
+
+                case 3:
+                    int fact = 1;
+                    int temp = Math.abs(number);
+                    int i = 1;
+
+                    while (i <= temp) {
+                        fact *= i;
+                        i++;
+                    }
+
+                    System.out.println("فاکتوریل عدد (به‌صورت قدرمطلق): " + fact);
+                    break;
+
+                case 4:
+                    System.out.println("خروج از برنامه.");
+                    break;
+
+                default:
+                    System.out.println("گزینه نامعتبر است!");
+            }
+        }
+
+    }
 }
